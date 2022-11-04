524798d71abfe7eaf7c910faff7ff52551cc7de8
AKANSASIRA <96227376+AKANSASIRA@users.noreply.github.com>
2022-11-04 21:13:19 +0300

    Add files via upload

---
+++ class_bottle.py
@@ -0,0 +1,18 @@
+#AKANSASIRA ISAAC.PY
+class Bottle:
+    #go a head and give your class attributes
+    def __init_(self,volume,area,type):
+        self.volume=volume
+        self.area=area
+        self.type=type
+        #define your attribues
+        object=Bottle(120,230,disposable_bottle)
+        print(object.area)
+        print(object.volume)
+
+        #GO a head and define other actions of yur object
+        def pour(self):
+            print("pouring")
+            def fill(self):
+                print("filling")
+
---
+++ classes.py
@@ -0,0 +1,19 @@
+# AKANSASIRA ISAAC CLASSES.PY
+class Person:
+    #go a head and give a class attributes
+    def__init_(self,age,weight,height,first_name,last_name,catch_phase):
+    self.age=age
+    self.weight=weight
+    self.height=height
+    self.first_name=first_name
+    self.last_name=last_name
+    self.catch_phase=catch_phase
+    #define your class attributes
+    user=person(22,70,122, "ISAAC", "AKANSASIRA", "YOU KNOW MORE")
+    print(user.age)
+    print(user.weight)
+    # you can go ahead and instruct a user to do certain actions
+    def walk(self):
+        print("walking......")
+        def sing(self):
+            print("singing......")
