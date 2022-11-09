57de2f84f06ca84c25df790d675565e683be44ff
AKANSASIRA <96227376+AKANSASIRA@users.noreply.github.com>
2022-11-09 09:41:12 +0300

    Add files via upload

--- class.py
+++ class.py
@@ -1,16 +1,24 @@
-class Person:
-    "This is a person class"
-    age = 10
+class Team:
+    def__init_(self,name,Stadium,Capacity,NetWorth,Years,NickName,logo,Legends,Players,MostValuedPlayers,WorldRecords,CurrentCoach,Colour,PremierLeagueTrophies,FATrophies,ChampionsLeagueAppearances):
+        self.name=name
+        self.Stadium=Stadium
+        self.Capacity=Capacity
+        self.NetWorth=NetWorth
+        self.Years=Years
+        self.NickName=NickName
+        self.logo=logo
+        self.Legends=Legends
+        self.Players=Players
+        self.MostValuedPlayers=MostValuedPlayers
+        self.WorldRecords=WorldRecords
+        self.CurrentCoach=CurrentCoach
+        self.Colour=Colour
+        self.PremierLeagueTrophies=PremierLeagueTrophies
+        self.FATrophies=FATrophies
+        self.ChampionsLeagueAppearances=ChampionsLeagueAppearances
 
-    def greet(self):
-        print('Hello')
+        def details(self):
+            print(self.MostValuedPlayers + "is the most valued" + self.name + "valued player")
 
-
-# Output: 10
-print(Person.age)
-
-# Output: <function Person.greet>
-print(Person.greet)
-
-# Output: "This is a person class"
-print(Person.__doc__)
+            Arsenal=Team("Arsenal", "EmiratesStadium", "2Million", "90BillionUsDollars", "120years+", "TheGunners", "GUN", "TieryHenry,VanPerse,Aubomeyang,SANCHEZ,Songo...", "saka,ramsdale,party,martinelli,ordegard(captain)", "Martinelli","UNBEATEN..GOLDENpremier","ARTETA", "red", "13", "14","131")
+            Arsenal.details()
